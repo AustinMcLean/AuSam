e9c0171751d868fb6e4a72c7ccba2a29a8992a0a
AustinMcLean <austin_mclean@hotmail.com>
2024-03-17 23:05:12 +0000

    Added basic databse intergation for RSVPs

--- Django_Demo/demo/myapp/models.py
+++ Django_Demo/demo/myapp/models.py
@@ -6,3 +6,7 @@ from django.db import models
 class TodoItem(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class RsvpEntry(models.Model):
+    name = models.CharField(max_length=200)
+    attending = models.CharField(max_length=20)
