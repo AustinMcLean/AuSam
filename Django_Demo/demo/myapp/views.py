dabe76fb80eb4a532cf202a7353931fb2c386743
Austin McLean <47283808+AustinMcLean@users.noreply.github.com>
2024-03-17 23:08:46 +0000

    Added basic databse intergation for RSVPs (#26)
    Co-authored-by: AustinMcLean <austin_mclean@hotmail.com>

--- Django_Demo/demo/myapp/views.py
+++ Django_Demo/demo/myapp/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import TodoItem
+from .models import TodoItem, RsvpEntry
 import json
 
 # Create your views here.
@@ -31,8 +31,34 @@ def submit_rsvp(request):
 
         # Process the data received from the front end
         name = request_body_json.get('full-name')
+        response = request_body_json.get('attending')
 
         # TODO: process and save data to the database
+        rsvp_entries = RsvpEntry.objects.all()
+        # rsvp_entries = []
+        print(rsvp_entries)
+
+        current_rsvp = None
+        for entry in rsvp_entries:
+            print(entry)
+            print(entry.name)
+            if name == entry.name:
+                entry.attending = response
+                current_rsvp = entry
+
+        if not current_rsvp:
+            # raise Exception('No invite found for given name.')
+            print('No invite found for given name.')
+
+        # current_rsvp = RsvpEntry()
+        # current_rsvp.name = name
+        # current_rsvp.attending = response
+        print(current_rsvp)
+        # rsvp_entries.appends(current_rsvp)
+        # print(rsvp_entries.name)
+
+        # Insert or Update DB
+        current_rsvp.save()
 
         return JsonResponse({'message': f'RSVP submitted successfully for {name}'})
     
