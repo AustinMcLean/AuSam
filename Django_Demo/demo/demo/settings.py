7d946e780727ddb0be809c2f2682152e80f769ba
Austin McLean <47283808+AustinMcLean@users.noreply.github.com>
2024-04-01 18:59:54 +0100

    Added session timeout (#31)
    * first attempt at adding session timeout
    
    * increased session timeout to 60 minutes
    
    ---------
    
    Co-authored-by: AustinMcLean <austin_mclean@hotmail.com>

--- Django_Demo/demo/demo/settings.py
+++ Django_Demo/demo/demo/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
+from django.conf import settings
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Session settings
+SESSION_COOKIE_AGE = 3600  # 60 minutes
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
