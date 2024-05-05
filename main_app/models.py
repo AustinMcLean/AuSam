76de5807320258a143a54d7cedf0e2e7633bf055
AustinMcLean <austin_mclean@hotmail.com>
2024-05-05 23:17:34 +0100

    Added new fields to databse model and the Javascript to display the values obtained from the databse

--- main_app/models.py
+++ main_app/models.py
@@ -4,5 +4,14 @@ from django.db import models
 
 class RsvpEntry(models.Model):
     name = models.CharField(max_length=200, db_index=True)
-    invite = models.IntegerField()
-    attending = models.CharField(max_length=20)
+    invite = models.IntegerField(null=True)
+    attending = models.CharField(max_length=20, null=True)
+    meal = models.CharField(max_length=100, null=True)
+    dietary_restrictions = models.CharField(max_length=200, null=True, blank=True)
+    babysitter = models.CharField(max_length=20, null=True)
+    welcome_dinner = models.CharField(max_length=20, null=True)
+    welcome_meal = models.CharField(max_length=50, null=True, blank=True)
+    pizza = models.CharField(max_length=20, null=True)
+    pizza_meal = models.CharField(max_length=50, null=True, blank=True)
+    stay = models.CharField(max_length=20, null=True)
+
