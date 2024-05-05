8823acc02c2195e34c5473914e5f4295cd00625a
AustinMcLean <austin_mclean@hotmail.com>
2024-05-06 00:57:36 +0100

    Added is_child field to database model and toggling display of babysitter question based on its value

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class RsvpEntry(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     invite = models.IntegerField(null=True)
+    is_child = models.BooleanField(default=False)
     attending = models.CharField(max_length=20, null=True)
     meal = models.CharField(max_length=100, null=True)
     dietary_restrictions = models.CharField(max_length=200, null=True, blank=True)
