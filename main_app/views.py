923495ed5c582b4ff5ac0db0a82598b0df0a5df2
AustinMcLean <austin_mclean@hotmail.com>
2024-05-08 23:27:19 +0100

    saving song-request to database

--- main_app/views.py
+++ main_app/views.py
@@ -99,6 +99,7 @@ def submit_rsvp(request):
             current_rsvp.attending = form.get('attending')
             current_rsvp.meal = form.get('food-preference')
             current_rsvp.dietary_restrictions = form.get('dietary-restrictions')
+            current_rsvp.song_request = form.get('song-request')
             current_rsvp.babysitter = form.get('babysitter')
             current_rsvp.welcome_dinner = form.get('welcome-dinner')
             current_rsvp.welcome_meal = form.get('welcome-meal')
