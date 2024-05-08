a24f6f1f5f5d3333f35b4a798cad547fc62bc61f
AustinMcLean <austin_mclean@hotmail.com>
2024-05-08 23:09:53 +0100

    Added song request field to database model and form + fancy styling

--- main_app/models.py
+++ main_app/models.py
@@ -3,12 +3,13 @@ from django.db import models
 # Create your models here.
 
 class RsvpEntry(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)
     invite = models.IntegerField(null=True)
     is_child = models.BooleanField(default=False)
     attending = models.CharField(max_length=20, null=True, blank=True)
     meal = models.CharField(max_length=100, null=True, blank=True)
-    dietary_restrictions = models.CharField(max_length=200, null=True, blank=True)
+    dietary_restrictions = models.CharField(max_length=100, null=True, blank=True)
+    song_request = models.CharField(max_length=200, null=True, blank=True)
     babysitter = models.CharField(max_length=20, null=True, blank=True)
     welcome_dinner = models.CharField(max_length=20, null=True, blank=True)
     welcome_meal = models.CharField(max_length=50, null=True, blank=True)
