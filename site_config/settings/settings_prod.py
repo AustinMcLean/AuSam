0a19f4cb13f9cc616a03f3a1e79a6b55ce70daf0
AustinMcLean <austin_mclean@hotmail.com>
2024-10-05 19:06:30 +0100

    trying with setting dj_database_url.config

--- site_config/settings/settings_prod.py
+++ site_config/settings/settings_prod.py
@@ -2,9 +2,6 @@ from .settings_base import *
 
 DEBUG = False
 
-# 'default': dj_database_url.config(default='postgres://localhost')
-
 DATABASES = {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
 }
